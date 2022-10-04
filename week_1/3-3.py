66650b1bb7b22729b7d9df29dd0d9a400a02598d
poodlepoodle <chammal97@naver.com>
2022-10-04 15:14:27 +0900

    :art: 3주차: 문제 풀이 디렉토리 구조 정

--- week_1/3-3.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-M, N = map(int, sys.stdin.readline().rstrip().split())
-numbers = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(M)]
-
-mins = [min(line) for line in numbers]
-print(max(mins))
